c50031ebdf5fb0b43d688bc72bf64f32b8888814
szymon112233 <szymmon123@gmail.com>
2025-10-28 21:00:25 +0100

    Added better Ui for values changes and interaction

--- UI/MainUI.cs
+++ UI/MainUI.cs
@@ -27,16 +27,38 @@ public partial class MainUI : Node
 	private void OnNotorietyChanged(int newValue)
 	{
 		NotorietyLabel.Text = $"Notoriety: {newValue}";
+		
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(NotorietyLabel, "theme_override_colors/font_color", Colors.Yellow, 0.1f);
+		tween.TweenProperty(NotorietyLabel, "theme_override_font_sizes/font_size", 33.0f, 0.1f);
+		tween.SetParallel();
+		tween.TweenProperty(NotorietyLabel, "theme_override_colors/font_color", Colors.White, 1.5f);
+		tween.TweenProperty(NotorietyLabel, "theme_override_font_sizes/font_size", 31.0f, 1.0f);
 	}
 
 	private void OnManaChanged(int newValue)
 	{
 		ManalLabel.Text = $"Mana: {newValue}";
+		
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(ManalLabel, "theme_override_colors/font_color", Colors.Yellow, 0.1f);
+		tween.TweenProperty(ManalLabel, "theme_override_font_sizes/font_size", 33.0f, 0.1f);
+		tween.SetParallel();
+		tween.TweenProperty(ManalLabel, "theme_override_colors/font_color", Colors.White, 1.5f);
+		tween.TweenProperty(ManalLabel, "theme_override_font_sizes/font_size", 31.0f, 1.0f);
+		
 	}
 
 	private void OnDivineApprovalChanged(int newValue)
 	{
 		DivinieApprovalLabel.Text = $"Divinie Approval: {newValue}";
+		
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(DivinieApprovalLabel, "theme_override_colors/font_color", Colors.Yellow, 0.1f);
+		// tween.TweenProperty(DivinieApprovalLabel, "theme_override_font_sizes/font_size", 20.0f, 0.1f);
+		// tween.SetParallel(true);
+		tween.TweenProperty(DivinieApprovalLabel, "theme_override_colors/font_color", Colors.White, 1.5f);
+		// tween.Parallel().TweenProperty(DivinieApprovalLabel, "theme_override_font_sizes/font_size", 10.0f, 1.0f);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
