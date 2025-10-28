8b74f234d9e567711575c84f2ccffc16fae24107
szymon112233 <szymmon123@gmail.com>
2025-10-28 19:17:41 +0100

    Added game manager, added Ui to display basic stats

---
+++ UI/MainUI.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class MainUI : Node
+{
+	[Export]
+	public Label DivinieApprovalLabel;
+	[Export]
+	public Label ManalLabel;
+	[Export]
+	public Label NotorietyLabel;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var gameManager = GameManager.Instance;
+		
+		gameManager.DivineApprovalChanged += OnDivineApprovalChanged;
+		gameManager.ManaChanged += OnManaChanged;
+		gameManager.NotorietyChanged += OnNotorietyChanged;
+	}
+
+	private void OnNotorietyChanged(int newValue)
+	{
+		NotorietyLabel.Text = $"Notoriety: {newValue}";
+	}
+
+	private void OnManaChanged(int newValue)
+	{
+		ManalLabel.Text = $"Mana: {newValue}";
+	}
+
+	private void OnDivineApprovalChanged(int newValue)
+	{
+		DivinieApprovalLabel.Text = $"Divinie Approval: {newValue}";
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
