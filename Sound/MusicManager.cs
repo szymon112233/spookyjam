c3ac67ef856d6642f44f36cfdc82fc7e9ff998fb
Godde <woodake@hotmail.com>
2025-10-31 15:50:18 +0100

    Ending music

--- Sound/MusicManager.cs
+++ Sound/MusicManager.cs
@@ -10,6 +10,9 @@ public partial class MusicManager : AudioStreamPlayer
 	private AudioStream[] notorietyTracks;
 	private int currentNotorietyTrack = 0;
 	
+	[Export]
+	private AudioStream[] endingTracks;
+	
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -20,8 +23,9 @@ public partial class MusicManager : AudioStreamPlayer
 			Stream = notorietyTracks[currentNotorietyTrack];
 			Play();
 		}
-			
-		
+
+		GameManager.Instance.GameEnded += GameEndingMusic;
+
 	}
 
 	private void IncreaseNotorietyTrack()
@@ -33,6 +37,18 @@ public partial class MusicManager : AudioStreamPlayer
 			Stream = notorietyTracks[currentNotorietyTrack];
 			Play();
 		}
+	}
+	
+	private void GameEndingMusic(GameManager.Ending ending)
+	{
+		Stop();
+		int indexNr = (int)ending;
+		if (indexNr >= endingTracks.Length)
+		{
+			throw new System.IndexOutOfRangeException("This endings track has not been added to the array I guess");
+		}
+		Stream = endingTracks[indexNr];
+		Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
